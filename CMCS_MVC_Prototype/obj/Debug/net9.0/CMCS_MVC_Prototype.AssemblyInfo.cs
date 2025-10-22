b3364ba990e98d5830fbc80abf2078dc24cb67f8
Fanelo Maluleke <st10445045@imconnect.edu.za>
2025-10-22 12:10:35 +0200

    homepage icons update
    removed homepage icon

--- CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
+++ CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92c520b1b0966a61c0dd4f7ba553fece9bd0975b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74560ea1b23dcd8c231addff83980f954393bbfc")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
