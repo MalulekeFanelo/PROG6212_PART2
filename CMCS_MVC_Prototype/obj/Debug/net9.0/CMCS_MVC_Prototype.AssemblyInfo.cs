616083ce6f65cb7f7e4ad2e4ba92bc1ae4e06055
Fanelo Maluleke <st10445045@imconnect.edu.za>
2025-10-22 12:34:36 +0200

    submit claim delete updated
    removed null  database information

--- CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
+++ CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+74560ea1b23dcd8c231addff83980f954393bbfc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3364ba990e98d5830fbc80abf2078dc24cb67f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
