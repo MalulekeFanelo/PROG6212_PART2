47d481e7f15f7089ecb3482c1b9384f1845a3e1f
Fanelo Maluleke <st10445045@imconnect.edu.za>
2025-11-20 16:22:54 +0200

    compare homepage views
    tested the differences between homepage styles

--- CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
+++ CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fce0f15d516bf178eb31a180eef55eea6f19b99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d8190f79dd6d2da131cff7a89d4d5a3813b7f32")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
