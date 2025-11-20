7fce0f15d516bf178eb31a180eef55eea6f19b99
Fanelo Maluleke <st10445045@imconnect.edu.za>
2025-11-20 16:15:24 +0200

    update homepage
    visual updates made to homepage

--- CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
+++ CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1d8e8823dbd52238e3d662003bac709fd77c73")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0dd27060d47d40bbea31c469c1a8efdd06930007")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
