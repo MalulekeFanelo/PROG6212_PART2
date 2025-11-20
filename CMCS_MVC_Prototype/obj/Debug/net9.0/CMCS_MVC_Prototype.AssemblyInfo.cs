0dd27060d47d40bbea31c469c1a8efdd06930007
Fanelo Maluleke <st10445045@imconnect.edu.za>
2025-11-20 15:57:38 +0200

    change made to claim index
    updated claim index to present different views for HR and lecturer

--- CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
+++ CMCS_MVC_Prototype/obj/Debug/net9.0/CMCS_MVC_Prototype.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+570de37754fa93073026638741bc3e84ffec31fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff1d8e8823dbd52238e3d662003bac709fd77c73")]
 [assembly: System.Reflection.AssemblyProductAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CMCS_MVC_Prototype")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
